02c39925ccb274e12af1f093d3e874cb1bdd3f2e
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2020-01-10 20:40:55 -0300

    Criação do domain(Entidade) Aluno

---
+++ api_aluno/src/main/java/com/api/aluno/domain/Aluno.java
@@ -0,0 +1,87 @@
+package com.api.aluno.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_aluno")
+	private Integer id;
+	
+	@Column(name = "nome_aluno", nullable = false)
+	private String nome;
+
+	@Column(name = "idade_aluno", nullable = false)
+	private Integer idade;
+
+	
+	public Aluno() {
+		
+	}
+
+	public Aluno(Integer id, String nome, Integer idade) {
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
