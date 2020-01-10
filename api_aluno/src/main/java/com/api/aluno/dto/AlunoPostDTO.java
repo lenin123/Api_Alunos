7100f65363b6b86debbb960c8a349ec34b4cf18e
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2020-01-10 20:41:46 -0300

    Criação das classes de manipulação dto de aluno

---
+++ api_aluno/src/main/java/com/api/aluno/dto/AlunoPostDTO.java
@@ -0,0 +1,48 @@
+package com.api.aluno.dto;
+
+import java.io.Serializable;
+
+import com.api.aluno.domain.Aluno;
+
+public class AlunoPostDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private Integer idade;
+	
+	public AlunoPostDTO() {
+		
+	}
+
+	public AlunoPostDTO(Aluno objAluno) {
+		id = objAluno.getId();
+		nome = objAluno.getNome();
+		idade = objAluno.getIdade();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+		
+}
