de13a0a5e60278500f831c5dc5eef7a7a5821fca
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2020-01-11 01:02:55 -0300

    Criação da base dinâmica para teste.

---
+++ api_aluno/src/main/java/com/api/aluno/services/DBService.java
@@ -0,0 +1,38 @@
+﻿package com.api.aluno.services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.aluno.dto.AlunoPostDTO;
+
+
+
+@Service
+public class DBService {
+
+	@Autowired
+	private AlunoService alunoService;
+	
+	public void instantiateTestDatabase() throws ParseException {	
+		
+		criarAlunoFake(Arrays.asList("Lênin Müller Smapaio", 23));
+		criarAlunoFake(Arrays.asList("Adre Luis Sampaio", 45));
+		criarAlunoFake(Arrays.asList("Adriana Müller", 44));
+	}
+
+	private void criarAlunoFake(List<Object> listObjAluno) throws ParseException {
+		AlunoPostDTO objAlunoPostDTO = new AlunoPostDTO();
+		objAlunoPostDTO.setId(null);
+		objAlunoPostDTO.setNome(listObjAluno.get(0).toString());
+		objAlunoPostDTO.setIdade(Integer.parseInt(listObjAluno.get(1).toString()));
+		
+		alunoService.insert(objAlunoPostDTO);
+	}
+
+}
